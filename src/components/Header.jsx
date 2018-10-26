fedbb0694338103ff0d626658a9c514ea0c1aa1d
Eduard <eduardkarapetyan1997@mail.ru>
2018-10-26 13:26:02 +0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Stats from './Stats';
+
+function Header(props) {
+    return(
+        <header>
+            <Stats todos={props.todos} />
+            <h1>{props.title}</h1>
+        </header>
+    );
+}
+
+export default Header;
